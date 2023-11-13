e3b3d755199cfa8ce1a260f2e9dcf48261121f6d
ghbaek <ghbaek@enliple.com>
2023-11-13 18:21:38 +0900

    init

---
+++ src/programers/Solve120883.java
@@ -0,0 +1,11 @@
+package programers;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120883?language=java
+ */
+public class Solve120883 {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        return answer;
+    }
+}
